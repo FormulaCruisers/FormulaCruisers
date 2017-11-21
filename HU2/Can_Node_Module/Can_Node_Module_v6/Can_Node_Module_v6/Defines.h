b8ebf59a36f76daefb1d3ba7ea61dadb538249a7
Jeremy Geels <a@jgw.tf>
2017-11-21 14:56:14 +0100

    Updated doc and cleaned up defines for can node

--- HU2/Can_Node_Module/Can_Node_Module_v6/Can_Node_Module_v6/Defines.h
+++ HU2/Can_Node_Module/Can_Node_Module_v6/Can_Node_Module_v6/Defines.h
@@ -6,39 +6,6 @@
 
 #define BAUD 500
 
-/*************** Node 1 ***************/
-#define STUURPOSITIE				0x11
-#define RPM_VOOR_LINKS				0x12
-#define RPM_VOOR_RECHTS				0x13
-
-/*************** Node 2 ***************/
-#define GAS_1						0x21
-#define GAS_2						0x22
-#define REM							0x23
-
-/*************** Node 3 ***************/
-#define FLOW_RICHTING_LINKS			0x31
-#define TEMP_LINKS					0x32
-
-/*************** Node 4 ***************/
-#define FLOW_RICHTING_RECHTS		0x41
-#define TEMP_RECHTS					0x42
-
-/*************** EcU 2 ***************/
-// Data request
-#define RPM_LINKS_ACHTER			0x51
-#define RPM_RECHTS_ACHTER			0x52
-#define DRAAIRICHTING_LINKS_ACHTER	0x53
-#define DRAAIRICHTING_RECHTS_ACHTER 0x54
-
-// Commando's
-#define RUN_ENABLE					0x55
-#define MOTOR_CONTROLLER			0x56
-#define BRAKELIGHT					0x57
-#define PRE_DISCHARGE				0x58
-#define PUMP						0x59
-
-
 #define MASTERID	0x317 // Transmit Adress Master
 #define NODEID1		0x201 // NODE ID 1	Steering Node
 #define NODEID2		0x202 // NODE ID 2	Pedalbox Node
